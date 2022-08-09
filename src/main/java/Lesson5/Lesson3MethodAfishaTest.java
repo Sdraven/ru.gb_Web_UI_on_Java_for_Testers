fe9031fdcadb7848b58089a3cc87bb71f656f4e8
Sdraven <kbitko@gmail.com>
2022-08-09 10:39:21 +0300

    Выполненное ДЗ по уроку 5 (Версия 1 с упавшими тестами)

---
+++ src/main/java/Lesson5/Lesson3MethodAfishaTest.java
@@ -0,0 +1,29 @@
+//package Lesson5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+
+public class Lesson3MethodAfishaTest {
+    public static void main(String[] args) {
+      WebDriverManager.chromedriver().setup();
+      WebDriver driver = new ChromeDriver();
+      driver.get("https://www.afisha.ru/");
+      driver.findElement(By.xpath("//input[@placeholder='Событие, актер, место']")).sendKeys("ширли мырли");
+     // WebDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1[.='Все развлечения Москвы']")));
+
+      driver.findElement(By.xpath("//div[.='Ширли-мырли']")).click();
+      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+      driver.quit();
+
+    }
+}
+
